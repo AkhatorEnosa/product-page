4220a3e65bacaf8adff199d20db4a59235602a0a
AkhatorEnosa <osa@Akhators-MacBook-Pro.local>
2024-06-02 10:06:13 +0100

    gitignore + some changes to Product View

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -4,11 +4,18 @@ import One from '../assets/1.jpeg'
 export const ProductView = () => {
   return (
     <div className=' flex h-full justify-center items-center px-48'>
-        <div className='grid grid-cols-2 gap-5 px-20'>
-            <div className='flex flex-col'>
-               <img src={One} alt="image" />
-               <div>
-                Several Images
+        <div className='grid grid-cols-2 gap-5 px-40'>
+            <div className='flex flex-col gap-2'>
+               <img src={One} alt="image" className='rounded-lg'/>
+               <div className='grid grid-cols-8 gap-2'>
+                <img src={One} alt="image" className='rounded-lg'/>
+                <img src={One} alt="image" className='rounded-lg'/>
+                <img src={One} alt="image" className='rounded-lg'/>
+                <img src={One} alt="image" className='rounded-lg'/>
+                <img src={One} alt="image" className='rounded-lg'/>
+                <img src={One} alt="image" className='rounded-lg'/>
+                <img src={One} alt="image" className='rounded-lg'/>
+                <img src={One} alt="image" className='rounded-lg'/>
                </div>
             </div>
             <div>
