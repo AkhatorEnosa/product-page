839044040f900c3491e8e4601365fe3f4fa0e23d
AkhatorEnosa <osa@Akhators-MacBook-Pro.local>
2024-06-06 16:39:40 +0100

    final commit?

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -11,13 +11,28 @@ import Eight from '../assets/8.jpeg'
 export const ProductView = () => {
 
   const [ click, setClick ] = useState(false);
-  const [ imgsrc, setImgsrc ] = useState('/src/assets/1.jpeg')
-console.log('before click =>', imgsrc)
+  const [ imgsrc, setImgsrc ] = useState(One);
+  const [ key, setKey ] = useState("one");
+
+  const [ num, setNum ] = useState(0);
+
   const handlePic = (e) => {
     setClick(!click)
     // setImgsrc()
     setImgsrc(e.target.src);
-    console.log('after click =>', imgsrc);
+    setKey(e.target.alt)
+  }
+
+  const handleNumAdd = () => {
+    setNum(num+1)
+  }
+
+  const handleNumMinus = () => {
+    if(num > 0){
+      setNum(num-1)
+    } else {
+      setNum(0)
+    }
   }
 
   return (
@@ -26,20 +41,20 @@ console.log('before click =>', imgsrc)
               <div className='flex flex-col w-full gap-2'>
                 <img src={imgsrc} alt="image" className='md:rounded-lg'/>
                 <div className='hidden md:grid grid-cols-8 gap-2'>
-                  <img src={One} alt="image" className={imgsrc === One ? 'rounded-lg active' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/>
-                  <img src={Two} alt="image" className='rounded-lg transition- all duration-200' onClick={handlePic}/>
-                  <img src={Three} alt="image" className='rounded-lg transition- all duration-200' onClick={handlePic}/>
-                  <img src={Four} alt="image" className='rounded-lg transition- all duration-200' onClick={handlePic}/>
-                  <img src={Five} alt="image" className='rounded-lg transition- all duration-200' onClick={handlePic}/>
-                  <img src={Six} alt="image" className='rounded-lg transition- all duration-200' onClick={handlePic}/>
-                  <img src={Seven} alt="image" className='rounded-lg transition- all duration-200' onClick={handlePic}/>
-                  <img src={Eight} alt="image" className='rounded-lg transition- all duration-200' onClick={handlePic}/> 
+                  <img src={One} alt="one" className={key === 'one' ? 'rounded-lg active transition-all duration-200' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/>
+                  <img src={Two} alt="two" className={key === 'two' ? 'rounded-lg active transition-all duration-200' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/>
+                  <img src={Three} alt="three" className={key === 'three' ? 'rounded-lg active transition-all duration-200' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/>
+                  <img src={Four} alt="four" className={key === 'four' ? 'rounded-lg active transition-all duration-200' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/>
+                  <img src={Five} alt="five" className={key === 'five' ? 'rounded-lg active transition-all duration-200' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/>
+                  <img src={Six} alt="six" className={key === 'six' ? 'rounded-lg active transition-all duration-200' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/>
+                  <img src={Seven} alt="seven" className={key === 'seven' ? 'rounded-lg active transition-all duration-200' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/>
+                  <img src={Eight} alt="eight" className={key === 'eight' ? 'rounded-lg active transition-all duration-200' : 'rounded-lg transition-all duration-200'} onClick={handlePic}/> 
                 </div>
               </div>
               <div className="flex justify-center items-center">
                 <div className='flex flex-col gap-4 lg:px-0 lg:py-0 px-8 py-6'>
                     <h3 className='tracking-wide font-medium'>NIKE</h3>
-                    <h1 className='text-4xl font-bold'>Air Jordan 3 Retro "Green Glow"</h1>
+                    <h1 className='text-3xl md:text-4xl font-bold'>Air Jordan 3 Retro "Green Glow"</h1>
                     <p className='mt-3'>Clean and supreme, the AJ3 returns with all of its classic style and grace. Quality leather in the upper—with that luxurious elephant print texture—combines with visible Nike Air in the sole to make a comfortable, everyday icon.</p>
                     <div className='flex md:flex-col justify-between md:justify-normal md:gap-3'>
                       <h2 className='text-2xl p-0 m-0 font-bold flex gap-4 items-center'>$190.50 <span className='text-xs bg-black text-white px-3 py-1 rounded-md font-bold'>50%</span></h2>
@@ -47,9 +62,9 @@ console.log('before click =>', imgsrc)
                     </div>
                     <div className='flex flex-col md:grid grid-cols-2 gap-5'>
                       <div className='grid grid-cols-3 font-bold shadow-md rounded-md bg-slate-100 md:bg-white'>
-                        <button className='text-2xl text-[#fe590c] hover:bg-slate-200 transition-all duration-100'>-</button>
-                        <input type="number" name="number" id="number" placeholder='0' className='py-4 md:py-0 text-center border bg-slate-100 md:bg-white md:border-white'/>
-                        <button className='text-[18px] text-[#fe590c] hover:bg-slate-200 transition-all duration-100'>+</button>
+                        <button className='text-2xl text-[#fe590c] hover:bg-slate-200 transition-all duration-100' onClick={handleNumMinus}>-</button>
+                        <input type="number" name="number" id="number" placeholder='0' value={num} className='py-4 md:py-0 text-center border bg-slate-100 md:bg-white md:border-white' readOnly={true}/>
+                        <button className='text-[18px] text-[#fe590c] hover:bg-slate-200 transition-all duration-100' onClick={handleNumAdd}>+</button>
                       </div>
                       <button className='flex gap-4 px-7 py-5 md:py-2 bg-[#FE590C] rounded-lg justify-center items-center font-bold'><span className="material-icons">shopping_cart</span>Add</button>
                     </div>
